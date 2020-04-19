3536782672b9264e8e932fbcb31a0c31a97d367f
Уткин Владислав <mihut1@yandex>
2020-04-19 16:11:50 +0500

    Разработан метод подсчёта разницы между днями.

--- src/main/java/PostResult.java
+++ src/main/java/PostResult.java
@@ -6,33 +6,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PostResult
- */
 public class PostResult extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public PostResult() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	private static int daysCalculation (HttpServletRequest request, HttpServletResponse response) throws IOException {
+		LocalDate startDate = LocalDate.MIN;
+	    LocalDate endDate = LocalDate.MAX; 
+	    	try {
+			startDate = LocalDate.parse(request.getParameter("startDate"));
+		    endDate = LocalDate.parse(request.getParameter("endDate"));
+			} catch(DateTimeParseException e) {
+	            response.setContentType("text/html");
+	            response.setCharacterEncoding("UTF-8");
+	            PrintWriter writer = response.getWriter();
+	            try {
+	                writer.println("<h1> Ошибка! </h1>");
+	                writer.println("<h3> Вы должны выбрать даты начала и конца поезки </h3>");
+	                writer.println("<form action=\"MainForm.html\" method=\"POST\">");
+	                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+	                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+	                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+	                writer.println("&nbsp;&nbsp;&nbsp;");
+	                writer.println("<input type=\"submit\" value=\"Ладно\" />");
+	                writer.println("</form>");            
+	            } finally {
+	            	writer.close();
+			}
+			}
+		    Period period = Period.between(startDate, endDate);
+		    int daysResult = period.getDays();
+		    return daysResult;
+		}
 
 }
