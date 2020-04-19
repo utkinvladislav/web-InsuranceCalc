f5496ad4ff0e65bc66848af0ff00fb4298276eb9
Уткин Владислав <mihut1@yandex>
2020-04-19 16:41:29 +0500

    Добавлен основоной метод.

--- src/main/java/AuthServ.java
+++ src/main/java/AuthServ.java
@@ -1,39 +1,51 @@
-
-
+import java.util.Scanner;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class authServ
- */
-public class AuthServ extends HttpServlet {
+import javax.servlet.http.HttpSession;
+  
+@WebServlet("/insurancyCalc")
+public class SimpleServlet extends HttpServlet {
+    static HttpSession session;
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public authServ() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+		session = request.getSession();
+		session.setMaxInactiveInterval(600);
+		boolean authSuccess = false;
+		String username;
+	    String password;
+		username = request.getParameter("username");
+	    password = request.getParameter("password");
+	    session.setAttribute("username", username);
+        authSuccess = authorization(username, password);
+        if (authSuccess == true) {
+        	response.sendRedirect("MainForm.html");
+        } else {  
+            response.setContentType("text/html");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter writer = response.getWriter();
+            try {
+                writer.println("<h1> Данные не совпадают! </h1>");
+                writer.println("<h2> Пожалуйста, повторите попытку </h2>");
+                writer.println("<form action=\"AuthForm.html\" method=\"POST\">");
+                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                writer.println("&nbsp;&nbsp;&nbsp;");
+                writer.println("<input type=\"submit\" value=\"Ладно\" />");
+                writer.println("</form>");
+            } finally {
+                writer.close();  
+            } 
+        }        
+    }    
 	boolean authorization(String incomingUsername, String incomingPassword) {
     	String username;  
     	String password;
