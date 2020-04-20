7a6f4442c5ecaacd30c3774f517bdda09b186b63
Уткин Владислав <mihut1@yandex>
2020-04-20 08:38:52 +0500

    Добавление комментариев.

--- src/main/java/PostResult.java
+++ src/main/java/PostResult.java
@@ -1,10 +1,11 @@
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.time.LocalDate; 
+import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
@@ -14,6 +15,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+ * Сервлет, главная функция которого — производить расчёт
+ * и выводить информацию
+ */
 @WebServlet("/result")
 public class PostResult extends HttpServlet {
 
@@ -26,9 +31,12 @@ public class PostResult extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
+        /*
+         * проверка успеха авторизации по атрибуту сессии
+         */
         try {
 	        if (AuthServ.session == null || AuthServ.session.getAttribute("username") == null) {
-	            try {
+	            try { //формирование сообщение об ошибке
 	                writer.println("<h1> Сначала нужно войти в систему! </h1>");
 	                writer.println("<form action=\"AuthForm.html\" method=\"POST\">");
 	                writer.println("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
@@ -42,7 +50,7 @@ public class PostResult extends HttpServlet {
 	            } 
 	        }
         }
-        catch (IllegalStateException e) {
+        catch (IllegalStateException e) { //перехват ошибки при истечении срока сессии
             try {
                 writer.println("<h1> Ваш сеанс закончился! </h1>");
                 writer.println("<h2> Повторите авторизацию </h2>");
@@ -57,9 +65,9 @@ public class PostResult extends HttpServlet {
                 writer.close();  
             } 
         }
-		int daysResult = daysCalculation(request, response);
+		int daysResult = daysCalculation(request, response); //подсчёт разницы между датами
 		if (daysResult <=1)
-			 try {
+			 try { //формирование сообщения об ошибке
 	                writer.println("<h1> Ошибка! </h1>");
 	                writer.println("<h3> Мы не выдаём полис меньше, чем на одни сутки </h3>");
 	                writer.println("<form action=\"MainForm.html\" method=\"POST\">");
@@ -72,9 +80,9 @@ public class PostResult extends HttpServlet {
 	            } finally {
 	                writer.close();   
 	            }
-		BigDecimal res = insCalculation( request, response);
-		if (res.compareTo(new BigDecimal(0)) == 0) {
-			 try {
+		BigDecimal res = insCalculation( request, response); //подсчёт стоимости
+		if (res.compareTo(new BigDecimal(0)) == 0) { //стоимость может равняться нулю, только если не указано число страхуемых
+			 try { //вывод сообщения об ошибке
 	                writer.println("<h1> Ошибка! </h1>");
 	                writer.println("<h3> Вы указали нулевое количество страхуемых </h3>");
 	                writer.println("<form action=\"MainForm.html\" method=\"POST\">");
@@ -88,7 +96,7 @@ public class PostResult extends HttpServlet {
 	                writer.close();   
 	            }
 		}
-            try {
+            try { //вывод результатов
                 writer.println("<h2> Результаты </h2>");
                 writer.println("<strong> Стоимость полиса в рублях: </strong>" + res);
                 writer.println("<br> </br>");
@@ -102,18 +110,20 @@ public class PostResult extends HttpServlet {
                 writer.close();   
             }
         }        
-
+/*
+ * метод подсчитывает разницу между днями с помощью инструментов LocalDate
+ */
 	private static int daysCalculation (HttpServletRequest request, HttpServletResponse response) throws IOException {
-		LocalDate startDate = LocalDate.MIN;
+		LocalDate startDate = LocalDate.MIN; //инициализация переменных
 	    LocalDate endDate = LocalDate.MAX; 
 	    	try {
-			startDate = LocalDate.parse(request.getParameter("startDate"));
-		    endDate = LocalDate.parse(request.getParameter("endDate"));
-			} catch(DateTimeParseException e) {
+			startDate = LocalDate.parse(request.getParameter("startDate")); //получение начальной даты
+		    endDate = LocalDate.parse(request.getParameter("endDate")); //получение конечной даты
+			} catch(DateTimeParseException e) { //ловля ошибки, возникающей при пустом календаре
 	            response.setContentType("text/html");
 	            response.setCharacterEncoding("UTF-8");
 	            PrintWriter writer = response.getWriter();
-	            try {
+	            try {  //формирование сообщения
 	                writer.println("<h1> Ошибка! </h1>");
 	                writer.println("<h3> Вы должны выбрать даты начала и конца поезки </h3>");
 	                writer.println("<form action=\"MainForm.html\" method=\"POST\">");
@@ -127,31 +137,40 @@ public class PostResult extends HttpServlet {
 	            	writer.close();
 			}
 			}
+	    	//создание объекта Period и получение его атрибута
 		    Period period = Period.between(startDate, endDate);
 		    int daysResult = period.getDays();
 		    return daysResult;
 		}
-	
+	/*
+	 * метод производит расчёт стоимости полиса.
+	 * Используется класс BigDecimal для расчётов в виду их финансового характера
+	 */
 	private static BigDecimal insCalculation (HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//объявление необходимых переменных
 		BigDecimal 
 			age1, age2, age3, age4, region, sport, lawyer, specialCase,
 			foresight, baggage, summMod, summ, promo, result, extra1,
 			extra2, extra3, extra4, extra5, k1, k2, k3, k4, days, multi, usable;
+		//Объявление и инициализация констант
 		final BigDecimal 
 		ageMod1 = new BigDecimal(1, MathContext.UNLIMITED),
 		ageMod2 = new BigDecimal(2.16, MathContext.UNLIMITED),
 		ageMod3 = new BigDecimal(2.54, MathContext.UNLIMITED),
 		ageMod4 = new BigDecimal(5, MathContext.UNLIMITED);
 			
-		days = new BigDecimal(Double.toString(daysCalculation(request, response)));
+		days = new BigDecimal(Double.toString(daysCalculation(request, response))); //получение числа дней
 		
+		//инициализация
 		region = new BigDecimal(0, MathContext.UNLIMITED);
 		lawyer = new BigDecimal(0, MathContext.UNLIMITED);
 		specialCase = new BigDecimal(0, MathContext.UNLIMITED);
 		sport = new BigDecimal(0, MathContext.UNLIMITED);
 		foresight = new BigDecimal(0, MathContext.UNLIMITED);
 		baggage = new BigDecimal(0, MathContext.UNLIMITED);
-
+		/*
+		 * Инициализация ряда переменных в зависимости от выбранного региона 
+		 */
 		String regionName = new String(request.getParameter("region"));
 		switch (regionName) {
 		case ("Шенгенская зона"):
@@ -187,13 +206,14 @@ public class PostResult extends HttpServlet {
 			baggage = new BigDecimal(36, MathContext.UNLIMITED);
 			break;
 		}
-
+		//инициализация
 		extra1 = new BigDecimal(0, MathContext.UNLIMITED);
 		extra2 = new BigDecimal(0, MathContext.UNLIMITED);
 		extra3 = new BigDecimal(0, MathContext.UNLIMITED);
 		extra4 = new BigDecimal(0, MathContext.UNLIMITED);
 		extra5 = new BigDecimal(0, MathContext.UNLIMITED);
 		
+		//присвоение единиц, если отмечен чекбокс
 		if (request.getParameter("extra1") != null ) 
 			extra1 = new BigDecimal(1, MathContext.UNLIMITED);
 		if (request.getParameter("extra2")!= null )
@@ -204,18 +224,27 @@ public class PostResult extends HttpServlet {
 			extra4 = new BigDecimal(1, MathContext.UNLIMITED);
 		if (request.getParameter("extra5")!= null )
 			extra5 = new BigDecimal(1, MathContext.UNLIMITED);
-
+		
+		//получение числа страхуемых каждого возраста
 		k1 = new BigDecimal(Double.parseDouble(request.getParameter("age1")), MathContext.UNLIMITED);
 		k2 = new BigDecimal(Double.parseDouble(request.getParameter("age2")), MathContext.UNLIMITED);
 		k3 = new BigDecimal(Double.parseDouble(request.getParameter("age3")), MathContext.UNLIMITED);
 		k4 = new BigDecimal(Double.parseDouble(request.getParameter("age4")), MathContext.UNLIMITED);
 		
+		//присвоение 2, если отмечен чекбокс, иначе — 1
 		multi = new BigDecimal(1, MathContext.UNLIMITED);
 		if (request.getParameter("multi")!= null )
 			multi = new BigDecimal(2, MathContext.UNLIMITED);
 		
+		/*
+		 * вызов метода проверки промокода
+		 */
 		PostResult calc = new PostResult();
 		promo = new BigDecimal(Double.toString(calc.promoCheck(request.getParameter("promo"))));	
+		
+		/*
+		 * выбор модификатора суммы в зависимости от выбранной пользователем опции
+		 */
 		summMod = new BigDecimal(0, MathContext.UNLIMITED);
 		switch (request.getParameter("summ")) {
 		case ("$35000"):
@@ -229,6 +258,7 @@ public class PostResult extends HttpServlet {
 			break;
 		}
 		
+		//переменная использованная для уменьшения объёма кода
 		usable = new BigDecimal(0, MathContext.UNLIMITED);
 		usable = usable.add(region);
 		usable = usable.add(sport.multiply(extra1));
@@ -237,6 +267,7 @@ public class PostResult extends HttpServlet {
 		usable = usable.add(foresight.multiply(extra4));
 		usable = usable.add(baggage.multiply(extra5));
 
+		//подсчёт результатов по каждому возрасту
 		age1 = new BigDecimal(0);
 		age1 = usable;
 		age1 = age1.multiply(summMod.multiply(ageMod1.multiply(days.multiply(k1))));
@@ -253,18 +284,25 @@ public class PostResult extends HttpServlet {
 		age4 = usable;
 		age4 = age4.multiply(summMod.multiply(ageMod4.multiply(days.multiply(k4))));
 
+		//подсчёт суммы
 		summ = new BigDecimal(0);
 		summ = age1.add(age2.add(age3.add(age4)));
-
+		
+		//подсчёт результата
 		result = new BigDecimal(0);
 		result = summ;
 		result = result.subtract(summ.multiply(promo));
 		result = result.multiply(multi);
-		result = result.setScale(2, RoundingMode.CEILING);
+		result = result.setScale(2, RoundingMode.CEILING); //округление до копеек в большую сторону
 
 		return result;
 	}	 
 
+	/*
+	 * метод получает промокод, введённый пользователем 
+	 * и сравнивает его с записанными в файле.
+	 * Если совпадение найдено, возвращается соответствующее значение
+	 */
 	private double promoCheck(String incomingPromo) {
     	String promo;
     	double promoVal;
